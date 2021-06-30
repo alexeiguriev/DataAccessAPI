671ff63252aee3423740324c94b9783e56ed03e6
alexeiguriev <gG4nmam85Z4aGpfZ>
2021-06-30 17:48:43 +0300

    Add project files.

---
+++ DataAccessAPI/DataAccessApi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccesslibrary.DataAccess;
+using DataAccesslibrary.Models;
+
+namespace DataAccessAPI
+{
+    public class DataAccessApi
+    {
+        SqlConnector sqlConn = new SqlConnector();
+
+        public string[][] GetAllUses()
+        {
+            List<USER> gotUsers = sqlConn.GetUSER_All();
+
+            List<string[]> list = new List<string[]>();
+
+            foreach (USER user in gotUsers)
+            {
+                list.Add(new[] { user.FirstName, user.LastName, user.EmailAddress, user.Password });
+            }
+
+            string[][] users = list.ToArray();
+            return users;
+        }
+
+        public void AddNewUser(string firstName, string lastName, string email, string password)
+        {
+            USER user = new USER();
+            user.FirstName = firstName;
+            user.LastName = lastName;
+            user.EmailAddress = email;
+            user.Password = password;
+            sqlConn.CreaUSER(user);
+        }
+
+        public void DeleteElement(string firstName, string lastName, string email, string password)
+        {
+            USER user = new USER();
+            user.FirstName = firstName;
+            user.LastName = lastName;
+            user.EmailAddress = email;
+            user.Password = password;
+            sqlConn.DeleteUSER(user);
+        }
+    }
+}
